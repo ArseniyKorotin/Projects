7d8d76074826a20f83cb9ce7b727e09c37e192a5
ArseniyKorotin <reyziktv@gmail.com>
2023-09-13 10:57:20 +0300

    hometask_3.3

---
+++ hometask_3.3/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <?php
+$x = rand(200, 400);
+echo "<div style='width: {$x}px; height: {$x}px; border-radius: 50%; margin: 10% auto; border: 2px solid;'></div>";
+?>
+
+</body>
+</html>
