8508f39fcd2c5709d663f33d0d18a5ef79bbfa93
ArseniyKorotin <reyziktv@gmail.com>
2023-11-05 15:05:21 +0200

    hometask_3.5_fix

--- 2.4/hometask_3.5/index.php
+++ 2.4/hometask_3.5/index.php
@@ -1,6 +1,6 @@
 <?php
-$str = "The quick $ green fox $ jumped over $ the lazy dog";
+$str = 'The quick $green fox $jumped over $the lazy dog';
 
-$st = preg_replace('/(\$ \w+)/', '<b>\1</b>', $str);
+$st = preg_replace('/\$(\w+)/', '<b>\1</b>', $str);
 
 echo $st;
