0442aeaecd4d19e6b8b91878dae04d55154a378b
ArseniyKorotin <reyziktv@gmail.com>
2023-11-12 22:14:00 +0200

    hometask_7.5

--- 2.5/hometask_7.5/index.php
+++ 2.5/hometask_7.5/index.php
@@ -5,7 +5,7 @@ function sum($carry, $item)
     return $carry += $item;
 }
 
-$array = [6, -7, -8, 9, -10, 11, 12];
+$array = [6, -7, -8, 9, -23, 11, 12];
 
-echo "Сума всіх елементів массива:\n";
+echo "Сума всіх елементів массива: \n";
 print_r(array_reduce($array, "sum"));
