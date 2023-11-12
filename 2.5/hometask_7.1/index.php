73355cd845bd37b638efd5b27f83919ad714e594
ArseniyKorotin <reyziktv@gmail.com>
2023-11-12 20:43:20 +0200

    hometask_7.2

---
+++ 2.5/hometask_7.1/index.php
@@ -0,0 +1,20 @@
+<?php
+function createBookCard($title, $authorFirstName, $authorLastName, $year, $publisher) {
+    $args = func_get_args();
+
+    foreach ($args as &$arg) {
+        $arg = $arg ?: '-';
+    }
+
+    list($title, $authorFirstName, $authorLastName, $year, $publisher) = $args;
+
+    $card = "Название книги: $title\n";
+    $card .= "Автор: $authorFirstName $authorLastName\n";
+    $card .= "Год написания: $year\n";
+    $card .= "Издательство: $publisher\n";
+
+    return $card;
+}
+
+$bookCard = createBookCard(null, "Джордж", null, 1954, "George Allen & Unwin");
+echo $bookCard;
