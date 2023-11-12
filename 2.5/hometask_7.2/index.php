91fe444f062880df3e27a8f321e4d7aafbbac135
ArseniyKorotin <reyziktv@gmail.com>
2023-11-12 21:39:42 +0200

    hometask_7.2

--- 2.5/hometask_7.2/index.php
+++ 2.5/hometask_7.2/index.php
@@ -1,9 +1,9 @@
 <?php
-function createBookCard($title, $authorFirstName, $authorLastName, $year, $publisher) {
+function createBookCard($title = "-", $authorFirstName = '-', $authorLastName = "-", $year = "-", $publisher = "-") {
     $args = func_get_args();
 
     foreach ($args as &$arg) {
-        $arg = $arg ?: '-';
+        $arg = $arg ?? '-';
     }
 
     list($title, $authorFirstName, $authorLastName, $year, $publisher) = $args;
