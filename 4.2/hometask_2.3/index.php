1dc8a94c7a10102a6ae00e4cad50c75621bb9dc2
ArseniyKorotin <reyziktv@gmail.com>
2024-03-17 15:05:13 +0200

    hometask_2.3_fixes

--- 4.2/hometask_2.3/index.php
+++ 4.2/hometask_2.3/index.php
@@ -2,9 +2,9 @@
 
 $data_birthday = [
     "Student1" => mktime(0, 0, 0, 5, 15, 1900),
-    "Student2" => mktime(0, 0, 0, 6, 3, 1979),
-    "Student3" => mktime(0, 0, 0, 8, 6, 1946),
-    "Student4" => mktime(0, 0, 0, 3, 3, 1932),
+    "Student2" => mktime(0, 0, 0, 6, 3, 1983),
+    "Student3" => mktime(0, 0, 0, 8, 6, 1937),
+    "Student4" => mktime(0, 0, 0, 3, 3, 1937),
     "Student5" => mktime(0, 0, 0, 5, 12, 1914),
     "Student6" => mktime(0, 0, 0, 8, 5, 1842),
     "Student7" => mktime(0, 0, 0, 3, 4, 1704),
@@ -25,13 +25,42 @@ $months = [
     9 => "Сентября",
     10 => "Октября",
     11 => "Ноября",
-    12 => "Декабря"
+    12 => "Декабря",
 ];
 
+function compare_birthdates($a, $b)
+{
+    $a_date = getdate($a);
+    $b_date = getdate($b);
 
+    // Сравниваем годы рождения
+    if ($a_date['year'] > $b_date['year']) {
+        return 1;
+    } else if ($a_date['year'] < $b_date['year']) {
+        return -1;
+    } else {
+        // Если годы равны, сравниваем месяцы рождения
+        if ($a_date['mon'] > $b_date['mon']) {
+            return 1;
+        } else if ($a_date['mon'] < $b_date['mon']) {
+            return -1;
+        } else {
+            // Если месяцы равны, сравниваем дни рождения
+            if ($a_date['mday'] > $b_date['mday']) {
+                return 1;
+            } else if ($a_date['mday'] < $b_date['mday']) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+    }
+}
+
+usort($data_birthday, 'compare_birthdates');
 
 foreach ($data_birthday as $key => $value) {
     $date_info = getdate($value);
     $month_name = $months[$date_info["mon"]];
-    echo $key.": ".$date_info["mday"]."/".$month_name."/".$date_info["year"]."<br> ";
+    echo $key . ": " . $date_info["mday"] . "/" . $month_name . "/" . $date_info["year"] . "<br> ";
 }
